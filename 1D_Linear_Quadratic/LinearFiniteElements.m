ff3231dd24d6529f9366dab5e54a80a236255635
spiroskou <90531367+spiroskou@users.noreply.github.com>
2022-05-29 12:43:00 +0300

    Update LinearFiniteElements.m

--- 1D_Linear_Quadratic/LinearFiniteElements.m
+++ 1D_Linear_Quadratic/LinearFiniteElements.m
@@ -19,7 +19,7 @@ f(x) = -(1-2*(2*x+cos(2*x)));    %%% fmass
 
 for i=1:2
     for j=1:2
-        ke(i,j) = int(diff(w(i),x)*diff(w(j),x)+w(i)*diff(w(j),x) -4* w(i)*w(j),x,0,h);
+        ke(i,j) = int(diff(w(i),x)*diff(w(j),x)+w(i)*diff(w(j),x)-4*w(i)*w(j),x,0,h);
     end
 end
 ke = vpa(subs(ke,h,he));
